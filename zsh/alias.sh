3d716108fa078656e90bf54873cbdace8ae67eaf
Fred <fred2167@gmail.com>
2022-02-26 14:04:53 -0800

    update alias.sh

---
+++ zsh/alias.sh
@@ -0,0 +1,7 @@
+alias ls="ls -G"
+alias grep="grep --color=auto"
+alias refresh=". ~/.zshrc"
+alias q=exit
+alias la="ls -a"
+alias wakehome="wakeonlan -i 192.168.1.255 -f ~/.ssh/WS_MAC"
+alias jppf="ssh -N -f -L 8877:localhost:8888 home"
