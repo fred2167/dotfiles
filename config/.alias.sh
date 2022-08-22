8a8a1ee7b58eb9fdb8b255814e5ee4d4ca82d884
Fred Chan <fredchan@Freds-MacBook-Pro.local>
2022-08-21 20:00:54 -0700

    add git alias

--- config/.alias.sh
+++ config/.alias.sh
@@ -4,3 +4,6 @@ alias refresh=". ~/.zshrc"
 alias q=exit
 alias la="ls -a"
 alias gitlog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit"
