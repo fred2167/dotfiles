cbb6ff79be426ae53abe33861bfb535ccced1453
Fred Chan <fredc@uber.com>
2022-10-23 17:04:49 -0700

    type

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-mdkdir $SCRIPT_DIR/bin
+mkdir $SCRIPT_DIR/bin
 cd $SCRIPT_DIR/bin
 
 cat $SCRIPT_DIR/list.txt | xargs -I % git clone --depth=1 %
