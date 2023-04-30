bf586c8bcb0f5f344f55870dc1c857642f160ea2
Fred Chan <fredchan@Freds-MacBook-Pro.local>
2023-04-29 20:21:10 -0700

    rg_fzf with code

--- shortcuts/shortcuts.sh
+++ shortcuts/shortcuts.sh
@@ -4,7 +4,8 @@ open_with_fzf() {
     fd -t f -H -I | fzf --multi --preview 'bat --color=always --style=numbers --line-range=:100 {}'| xargs -ro code
 }
 cd_with_fzf() {
-    cd $HOME && cd "$(fd -t d | fzf --preview="tree -L 1 {}" --bind="space:toggle-preview")"
+    cd $HOME 
+    cd "$(fd -t d | fzf --preview="tree -L 1 {}" --bind="space:toggle-preview")"
 }
 
 rg_fzf(){
@@ -25,13 +26,13 @@ fzf --ansi \
     --header '╱ CTRL-R (ripgrep mode) ╱ CTRL-F (fzf mode) ╱' \
     --preview 'bat --color=always {1} --highlight-line {2}' \
     --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
-    --bind 'enter:become(vim {1} +{2})'
+    --bind 'enter:become(code --goto {1}:{2}:{3})'
 }
 
 # bind shortcusts to control keys
 zle -N cd_with_fzf cd_with_fzf
 zle -N open_with_fzf open_with_fzf
 zle -N rg_fzf rg_fzf
-bindkey ^C cd_with_fzf
+bindkey ^G cd_with_fzf
 bindkey ^O open_with_fzf
 bindkey ^F rg_fzf
