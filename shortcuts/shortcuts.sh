2b28ef2c653d70a97054b399a9656918074ca60f
Fred Chan <fredchan@Freds-MacBook-Pro.local>
2023-05-03 19:13:16 -0700

    change goto

--- shortcuts/shortcuts.sh
+++ shortcuts/shortcuts.sh
@@ -3,9 +3,9 @@
 open_with_fzf() {
     fd -t f -H -I | fzf --multi --preview 'bat --color=always --style=numbers --line-range=:100 {}'| xargs -ro code
 }
-cd_with_fzf() {
-    cd $HOME
-    cd "$(fd -t d | fzf --preview="tree -L 1 {}" --bind="space:toggle-preview")"
+
+gt() {
+    cd "$(fd . $HOME -t d | fzf --preview="tree -L 1 {}")"
 }
 
 rf(){
@@ -30,7 +30,5 @@ rf(){
 }
 
 # bind shortcusts to control keys
-zle -N cd_with_fzf cd_with_fzf
 zle -N open_with_fzf open_with_fzf
-bindkey ^G cd_with_fzf
 bindkey ^O open_with_fzf
