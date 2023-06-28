9c9b91b63e37f5d4b36125a2e8323e0b95154dae
fredc <fredc@uber.com>
2023-06-28 00:02:54 +0000

    Revert "change zsh autosuggest to autocomplete"
    This reverts commit 4ff13304b608dd04ae26a16f94dd1adc3873f6e4.

--- zsh/zsh_tools_init.sh
+++ zsh/zsh_tools_init.sh
@@ -3,7 +3,7 @@
 ZSHTOOLS=$(dirname -- "$0")/plugins
 source $ZSHTOOLS/powerlevel10k/powerlevel10k.zsh-theme
 source $ZSHTOOLS/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-source $ZSHTOOLS/zsh-autocomplete/zsh-autocomplete.plugin.zsh     
+source $ZSHTOOLS/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $ZSHTOOLS/zsh-history-substring-search/zsh-history-substring-search.zsh
 source $ZSHTOOLS/zsh-you-should-use/you-should-use.plugin.zsh
 source ~/.fzf.zsh
