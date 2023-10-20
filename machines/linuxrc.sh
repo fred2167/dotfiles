3a96c19a8271e45860b46b9533186c78ca24f639
fred chan <fredc@uber.com>
2023-10-20 12:09:38 -0700

    add find and replace short cut

--- machines/linuxrc.sh
+++ machines/linuxrc.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 alias fd="fdfind"
 export FZF_DEFAULT_COMMAND='fdfind'
+
+# find and replace
+fr() {
+    rg "$1" --files-with-matches -0 | xargs -0 sed -i "s/$1/$2/g"
+}
--- machines/macrc.sh
+++ machines/macrc.sh
@@ -2,3 +2,8 @@
 export FZF_DEFAULT_COMMAND='fd'
 alias uuidgen="uuidgen | tr '[A-Z]' '[a-z]' | tr -d '\n' | tee >(pbcopy)"
 alias now="date +%s000 | tr -d '\n' | tee >(pbcopy)"
+
+# find and replace
+fr() {
+    rg "$1" --files-with-matches -0 | xargs -0 sed -i "" "s/$1/$2/g"
+}
