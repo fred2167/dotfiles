a884860ca9e73ffa695b7e3a7b50563fc378142e
fred chan <fredc@uber.com>
2023-12-11 20:39:58 -0800

    add plugin

--- zsh/zsh_tools_init.sh
+++ zsh/zsh_tools_init.sh
@@ -6,6 +6,7 @@ source $ZSHTOOLS/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
 source $ZSHTOOLS/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $ZSHTOOLS/zsh-history-substring-search/zsh-history-substring-search.zsh
 source $ZSHTOOLS/zsh-you-should-use/you-should-use.plugin.zsh
+source $ZSHTOOLS/jq-zsh-plugin/jq.plugin.zsh
 source ~/.fzf.zsh
 
 # zsh substring search key binding
