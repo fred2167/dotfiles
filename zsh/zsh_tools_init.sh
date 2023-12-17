f2e4c51ae334904388108cea329146f643f5dcac
Fred Chan <fred2167@gmail.com>
2023-12-17 15:14:10 -0800

    add jq-complete key binding

--- zsh/zsh_tools_init.sh
+++ zsh/zsh_tools_init.sh
@@ -15,5 +15,7 @@ bindkey '^[[B' history-substring-search-down
 bindkey "$terminfo[kcuu1]" history-substring-search-up
 bindkey "$terminfo[kcud1]" history-substring-search-down
 
+bindkey '^j' jq-complete
+
 # fzf configs
 eval "$(zoxide init zsh)"
